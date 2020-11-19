f1bd933425b6d408e5aab487cbc69e0fb2791bc0
Lalit Moharana <46365710+lalit2001@users.noreply.github.com>
2020-11-19 10:18:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { Switch,Route, Redirect} from 'react-router-dom';
 
 function App() {
   return (
-    const WP='https://wa.me/9114813691';
     <>
     <Navbar />
       <Switch>
@@ -22,7 +21,6 @@ function App() {
         <Route exact path='/about' component={About} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/service' component={Service} />
-        <Route exact path='/con' component={WP} />
         <Redirect to='/' />
       </Switch>
       <SocialFollow/>
